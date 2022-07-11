6f8dcfb716b117dd22743f25106021a36cb741aa
Telmoneiva <47387381+Telmoneiva@users.noreply.github.com>
2022-07-11 14:29:56 +0100

    Add files via upload
    Coded in 2017- Spelling Program  
    Language- C#

---
+++ NinjaSpelling/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace NinjaSpelling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startBut = new System.Windows.Forms.Button();
+            this.gameScreen = new System.Windows.Forms.Panel();
+            this.txbLetter = new System.Windows.Forms.GroupBox();
+            this.butEnter = new System.Windows.Forms.Button();
+            this.textLetter = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.labRemainingLives = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbWord = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.scoreScreen = new System.Windows.Forms.Panel();
+            this.lblTotalW = new System.Windows.Forms.Label();
+            this.lblTotalC = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gameScreen.SuspendLayout();
+            this.txbLetter.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.scoreScreen.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // startBut
+            // 
+            this.startBut.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startBut.Location = new System.Drawing.Point(12, 463);
+            this.startBut.Name = "startBut";
+            this.startBut.Size = new System.Drawing.Size(893, 112);
+            this.startBut.TabIndex = 3;
+            this.startBut.Text = "S T A R T";
+            this.startBut.UseVisualStyleBackColor = true;
+            this.startBut.Click += new System.EventHandler(this.startBut_Click);
+            // 
+            // gameScreen
+            // 
+            this.gameScreen.Controls.Add(this.txbLetter);
+            this.gameScreen.Controls.Add(this.groupBox2);
+            this.gameScreen.Controls.Add(this.groupBox1);
+            this.gameScreen.Controls.Add(this.txbWord);
+            this.gameScreen.Controls.Add(this.label1);
+            this.gameScreen.Location = new System.Drawing.Point(12, 12);
+            this.gameScreen.Name = "gameScreen";
+            this.gameScreen.Size = new System.Drawing.Size(893, 445);
+            this.gameScreen.TabIndex = 2;
+            this.gameScreen.Visible = false;
+            // 
+            // txbLetter
+            // 
+            this.txbLetter.Controls.Add(this.butEnter);
+            this.txbLetter.Controls.Add(this.textLetter);
+            this.txbLetter.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbLetter.ForeColor = System.Drawing.Color.DimGray;
+            this.txbLetter.Location = new System.Drawing.Point(57, 134);
+            this.txbLetter.Name = "txbLetter";
+            this.txbLetter.Size = new System.Drawing.Size(522, 298);
+            this.txbLetter.TabIndex = 4;
+            this.txbLetter.TabStop = false;
+            this.txbLetter.Text = "Write A Letter ";
+            // 
+            // butEnter
+            // 
+            this.butEnter.ForeColor = System.Drawing.Color.DarkOrange;
+            this.butEnter.Location = new System.Drawing.Point(6, 101);
+            this.butEnter.Name = "butEnter";
+            this.butEnter.Size = new System.Drawing.Size(470, 64);
+            this.butEnter.TabIndex = 1;
+            this.butEnter.Text = "R E P E A T    T H E   P H R A S E  ";
+            this.butEnter.UseVisualStyleBackColor = true;
+            this.butEnter.Click += new System.EventHandler(this.butEnter_Click);
+            // 
+            // textLetter
+            // 
+            this.textLetter.Location = new System.Drawing.Point(6, 31);
+            this.textLetter.MaxLength = 20;
+            this.textLetter.Name = "textLetter";
+            this.textLetter.Size = new System.Drawing.Size(470, 32);
+            this.textLetter.TabIndex = 0;
+            this.textLetter.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textLetter.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textLetter_KeyPress);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.labRemainingLives);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.Color.Red;
+            this.groupBox2.Location = new System.Drawing.Point(608, 286);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(238, 146);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Remaining Lives";
+            // 
+            // labRemainingLives
+            // 
+            this.labRemainingLives.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labRemainingLives.ForeColor = System.Drawing.Color.Red;
+            this.labRemainingLives.Location = new System.Drawing.Point(11, 56);
+            this.labRemainingLives.Name = "labRemainingLives";
+            this.labRemainingLives.Size = new System.Drawing.Size(221, 29);
+            this.labRemainingLives.TabIndex = 0;
+            this.labRemainingLives.Text = "5";
+            this.labRemainingLives.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.LimeGreen;
+            this.groupBox1.Location = new System.Drawing.Point(608, 134);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(238, 146);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Remaining Time";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(6, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(226, 29);
+            this.label2.TabIndex = 0;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txbWord
+            // 
+            this.txbWord.Enabled = false;
+            this.txbWord.Location = new System.Drawing.Point(57, 68);
+            this.txbWord.Name = "txbWord";
+            this.txbWord.Size = new System.Drawing.Size(789, 20);
+            this.txbWord.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DimGray;
+            this.label1.Location = new System.Drawing.Point(52, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Identify The Scret Answer ";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // scoreScreen
+            // 
+            this.scoreScreen.Controls.Add(this.lblTotalW);
+            this.scoreScreen.Controls.Add(this.lblTotalC);
+            this.scoreScreen.Controls.Add(this.label3);
+            this.scoreScreen.Location = new System.Drawing.Point(12, 12);
+            this.scoreScreen.Name = "scoreScreen";
+            this.scoreScreen.Size = new System.Drawing.Size(893, 123);
+            this.scoreScreen.TabIndex = 4;
+            this.scoreScreen.Visible = false;
+            // 
+            // lblTotalW
+            // 
+            this.lblTotalW.AutoSize = true;
+            this.lblTotalW.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalW.ForeColor = System.Drawing.Color.Red;
+            this.lblTotalW.Location = new System.Drawing.Point(17, 77);
+            this.lblTotalW.Name = "lblTotalW";
+            this.lblTotalW.Size = new System.Drawing.Size(51, 20);
+            this.lblTotalW.TabIndex = 2;
+            this.lblTotalW.Text = "label5";
+            // 
+            // lblTotalC
+            // 
+            this.lblTotalC.AutoSize = true;
+            this.lblTotalC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalC.ForeColor = System.Drawing.Color.Green;
+            this.lblTotalC.Location = new System.Drawing.Point(17, 47);
+            this.lblTotalC.Name = "lblTotalC";
+            this.lblTotalC.Size = new System.Drawing.Size(51, 20);
+            this.lblTotalC.TabIndex = 1;
+            this.lblTotalC.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 26);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "SCORE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 584);
+            this.Controls.Add(this.scoreScreen);
+            this.Controls.Add(this.startBut);
+            this.Controls.Add(this.gameScreen);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gameScreen.ResumeLayout(false);
+            this.gameScreen.PerformLayout();
+            this.txbLetter.ResumeLayout(false);
+            this.txbLetter.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.scoreScreen.ResumeLayout(false);
+            this.scoreScreen.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBut;
+        private System.Windows.Forms.Panel gameScreen;
+        private System.Windows.Forms.GroupBox txbLetter;
+        private System.Windows.Forms.Button butEnter;
+        private System.Windows.Forms.TextBox textLetter;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label labRemainingLives;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbWord;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel scoreScreen;
+        private System.Windows.Forms.Label lblTotalW;
+        private System.Windows.Forms.Label lblTotalC;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ NinjaSpelling/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NinjaSpelling
+{
+    public partial class Form1 : Form
+    {
+        // declare variables to use for the list of phrases
+        private string phrase;
+        private string[] words;
+        private string hideWord;
+
+        // declare variables to use for the timer
+        private int secs = 120;
+        private TimeSpan t;
+
+        // declare variable to use for the score and lifes
+        private int totalCorrect = 0;
+        private int totalWrong = 0;
+        private int attemptsLife = 5;
+
+        // declare variable to use to speeak
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            synthesizer.Volume = 100;  // 0...100
+            synthesizer.Rate = -2;     // -10...10
+            SetTimer();
+        }
+
+        // initialize timer from 2min
+        private void SetTimer()
+        {
+            secs = 120;
+            t = TimeSpan.FromSeconds(secs);
+            label2.Text = $"{t.Minutes.ToString().PadLeft(2, '0')}:{t.Seconds.ToString().PadLeft(2, '0')}";
+            timer1.Enabled = true;
+        }
+
+        private void startBut_Click(object sender, EventArgs e)
+        {
+            gameScreen.Visible = true;
+            startBut.Visible = false;
+            scoreScreen.Visible = false;
+            attemptsLife = 5;
+            ClearPhrase();
+            SetTimer();
+        }
+
+        // get new phrase from the list
+        private void ClearPhrase()
+        {
+
+            var fullPath = $"{AppDomain.CurrentDomain.BaseDirectory}Files\\wordlist.txt";
+            var s = GetPhrase(fullPath);
+            txbWord.Text = s;
+            labRemainingLives.Text = (attemptsLife).ToString();
+            textLetter.Text = string.Empty;
+        }
+
+        // get new phrase from the list
+        private string GetPhrase(string pathSource)
+        {
+            List<string> lisPrhases = new List<string>();
+            using (StreamReader sr = new StreamReader(pathSource))
+            {
+                while (sr.Peek() >= 0) // make the porgam pick the next phrase
+                {
+                    string line = sr.ReadLine();
+                    lisPrhases.Add(line);
+                }
+            }
+
+            var x = lisPrhases.Count();
+            var r = new Random();
+            var n = r.Next(0, x - 1);
+
+            phrase = lisPrhases.ToArray()[n];
+
+            words = phrase.Split(' ');
+            phrase = string.Empty;
+
+            hideWord = words[0];
+
+            for (int i = 2; i < words.Length; i++)
+            {
+                phrase += $"{words[i]} ";
+            }
+
+            phrase = phrase.Trim();
+            words = phrase.Split(' ');
+
+            synthesizer.SpeakAsync(phrase);
+
+            return phrase.ToLower().Replace(hideWord, "_");
+        }
+
+        // check typed word
+        private void textLetter_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                var dword = textLetter.Text;
+                if (dword.ToLower().Equals(hideWord))
+                {
+                    // set successfully message depend on level
+                    var message = string.Empty;
+                    switch (attemptsLife)
+                    {
+                        case 4:
+                            message = "brillant, keep yourself in the lead";
+                            break;
+                        case 3:
+                            message = "Congratulations, you're the super sumo of nimja spelling";
+                            break;
+                        case 2:
+                            message = "keep going, never give up";
+                            break;
+                        case 1:
+                            message = "you've almost lost pal";
+                            break;
+                    }
+
+                    synthesizer.SpeakAsync(message);
+                    txbWord.Text = txbWord.Text.Replace("_", dword);
+                    ClearPhrase();
+                    totalCorrect++;
+                }
+                else
+                {
+                    // if the word is not correct, take one life and check if it's the last one to end the game and show the scores
+                    attemptsLife -= 1;
+                    labRemainingLives.Text = (attemptsLife).ToString();
+                    totalWrong++;
+                    if (attemptsLife == 0)
+                    {
+                        synthesizer.Speak("Game Over, Baby!");
+                        timer1.Enabled = false;
+                        gameScreen.Visible = false;
+                        startBut.Visible = true;
+                        scoreScreen.Visible = true;
+                        lblTotalC.Text = $"Correct: {totalCorrect}";
+                        lblTotalW.Text = $"Wrong: {totalWrong}";
+                    }
+                }
+            }
+        }
+
+        private void butEnter_Click(object sender, EventArgs e)
+        {
+            // repeat the phrase
+            synthesizer.SpeakAsync(phrase);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // remove one second from the timer
+            secs -= 1;
+            t = TimeSpan.FromSeconds(secs);
+            label2.Text = $"{t.Minutes.ToString().PadLeft(2, '0')}:{t.Seconds.ToString().PadLeft(2, '0')}";
+
+            // if time is out end the game and show the scores
+            if (secs.Equals(0))
+            {
+                timer1.Enabled = false;
+                synthesizer.Speak("Time Out!");
+                scoreScreen.Visible = true;
+                lblTotalC.Text = $"Correct: {totalCorrect}";
+                lblTotalW.Text = $"Wrong: {totalWrong}";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
